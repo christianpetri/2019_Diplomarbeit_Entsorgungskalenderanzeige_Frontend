8f3f0a0750e7cb483cff001131dfca0ded17e0af
christianpetri <christianpetri@hotmail.com>
2019-05-06 11:36:54 +0200

    Changed to a diffrent IDE

---
+++ MikrokontrollerCode/src/userConfiguration.h
@@ -0,0 +1,26 @@
+#ifndef _USERCONFIGURATION_H_
+#define _USERCONFIGURATION_H_
+#define WIFI_NAME "CP"
+#define WIFI_PASSWORD "ChristianPetri1988"
+#define CIRCLE_ID 3
+
+#define HOST "www.entsorgungskalenderanzeige.christianpetri.ch"
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+#define FINGERPRINT "9620a9ed9756297500f628c9ce9a1e093e3658ae"; //certificate --> until Sunday, 21. July 2019
+
+#endif // _USERCONFIGURATION_H_
+
+/**
+EXAMPLE CONFIG
+Make sure that the "" are set correctly.  
+
+#define WIFI_NAME "MyAwsomeWifi" 
+#define WIFI_PASSWORD "ThereIsNoSpoon" 
+#define CIRCLE_ID 3
+
+#define HOST "www.entsorgungskalenderanzeige.christianpetri.ch"
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+#define FINGERPRINT "9620a9ed9756297500f628c9ce9a1e093e3658ae";
+ * */
